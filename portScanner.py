bf6b72582356a89be2508415291be07d51daf795
Nabeel Fakhar <67518699+Phreak971@users.noreply.github.com>
2021-12-20 16:29:41 -0500

    Update portScanner.py

--- portScanner.py
+++ portScanner.py
@@ -1,105 +1,128 @@
-import sys as sys
-import socket as socket
-import numpy as np
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+import optparse
+from scapy.all import *
+import sys
 
-
-class scanner:
-    """
-    This class contains all implenetation for port scanner
-    """
-
-    def __init__(self):
-        # These are favourite tcp ports that Nmap scans automatically.
-        self.portList = np.array([80, 23, 443, 21, 22, 25, 3389, 110, 445, 139,
+TCPportList = [80, 23, 443, 21, 22, 25, 3389, 110, 445, 139,
                                   143, 53, 135, 3306, 8080, 1723, 111, 995, 993, 5900,
                                   1025, 587, 8888, 199, 1720,
-                                  113, 554, 256])
+                                  113, 554, 256]
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-p', '--port', dest='portStart', help='Port to scan')
+    parser.add_option('-r', '--portEnd', dest='portEnd', help='Port Range End')
+    parser.add_option('-S', '--stealth', dest='stealthScan', help='Stealth Scan or Half Scan', action= 'store_true')
+    parser.add_option('-X', '--xmas', dest='xmasScan', help='Xmas Scan', action= 'store_true')
+    return parser.parse_args()[0]
 
-    def favouriteScan(self, ip):
-        # Scans from port list
-        print(f'Port Scanner Report for: {ip}')
-        closed = 0
-        for port in self.portList:
-            try:
-                tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def XmasScan(ip, port):
+    src_port = 0
+    closed = 0
+    filtered = 0
+    dst_ip = ip
+    if not port:
+        port = TCPportList
+    elif len(port) == 2:
+        port.extend(range(port[0], port[1]))
+        port = port[2:]
+    elif len(port) == 1:
+        pass
+    else:
+        return
+    for dst_port in port:
+        xmas_scan_resp = sr1(IP(dst=dst_ip)/TCP(dport=dst_port,flags="FPU"),timeout=0.5, verbose=0)
+        if (str(type(xmas_scan_resp))=="<type 'NoneType'>"):
+            print "[*] %d open|filtered" % dst_port
+        elif(xmas_scan_resp.haslayer(TCP)):
+            if(xmas_scan_resp.getlayer(TCP).flags == 0x14):
+                print "Closed"
+        elif(xmas_scan_resp.haslayer(ICMP)):
+            if(int(xmas_scan_resp.getlayer(ICMP).type)==3 and int(xmas_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+                filtered = filtered + 1
+    if closed:
+        print "[*] %d scanned ports closed" % closed
+    if filtered:
+        print "[*] %d scanned ports filtered" % filtered
+def StealthScan(ip, port):
+    src_port = 0
+    closed = 0
+    filtered = 0
+    dst_ip = ip
+    if not port:
+        port = TCPportList
+    elif len(port) == 2:
+        port.extend(range(port[0], port[1]))
+        port = port[2:]
+    elif len(port) == 1:
+        pass
+    else:
+        return
+    for dst_port in port:
+            stealth_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=0.5, verbose=0)
+            if(str(type(stealth_scan_resp)) == "<type 'NoneType'>"):
+                filtered = filtered + 1 
+            elif(stealth_scan_resp.haslayer(TCP)):
+                if(stealth_scan_resp.getlayer(TCP).flags == 0x12):
+                    send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="R"),timeout=0.5, verbose=0)
+                    print "[*] %d open" % dst_port
+                elif (stealth_scan_resp.getlayer(TCP).flags == 0x14):
+                    closed = closed + 1
+                elif(stealth_scan_resp.haslayer(ICMP)):
+                    if(int(stealth_scan_resp.getlayer(ICMP).type)==3 and int(stealth_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+                        filtered = filtered + 1 
+    if closed:
+        print "[*] %d scanned ports closed" % closed
+    if filtered:
+        print "[*] %d scanned ports filtered" % filtered
+def FullScan(ip, port):
+    src_port = 0
+    closed = 0
+    dst_ip = ip
+    if not port:
+        port = TCPportList
+    elif len(port) == 2:
+        port.extend(range(port[0], port[1]))
+        port = port[2:]
+    elif len(port) == 1:
+        pass
+    else:
+        return
+    for dst_port in port:
+        tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=2, verbose=0)
+        if(str(type(tcp_connect_scan_resp))=="<type 'NoneType'>"):
+            closed = closed + 1
+        elif(tcp_connect_scan_resp.haslayer(TCP)):
+                if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
+                    send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=2, verbose=0)
+                    print "[*] %d open" % dst_port
+                elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
+                    closed = closed + 1
+    if closed:
+        print "[*] %d scanned ports closed" % closed
 
-                if tcp.connect_ex((ip, port)) == 0:
-                    print(f'{port}/tcp open')
-                    tcp.close()
-                else:
-                    closed += 1
-                    tcp.close()
-
-            except Exception:
-                pass
-
-        print(f'{closed} scanned ports closed')
-
-    def rangeScan(self, ip, portStart, portEnd):
-        # Scan from range of ports given by user
-        print(f'Port Scanner Report for: {ip}')
-        closed = 0
-        for port in range(portStart, portEnd + 1):
-            try:
-
-                tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-                if tcp.connect_ex((ip, port)) == 0:
-                    print(f'{port}/tcp open')
-                    tcp.close()
-                else:
-                    closed += 1
-                    tcp.close()
-
-            except Exception:
-                pass
-        print(f'{closed} scanned ports closed')
-
-    def portScan(self, ip, port):
-        # Scan a specific port on the host
-        print(f'Port Scanner Report for: {ip}')
-        try:
-            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            if tcp.connect_ex((ip, port)) == 0:
-                print(f'{port}/tcp open')
-                tcp.close()
-            else:
-                print(f'{port}/tcp closed')
-                tcp.close()
-        except Exception:
-            pass
-
-
-def help():
-    print('Usage\nportScanner.py [ip]')
-    print('portScanner.py [ip] [port Specific]')
-    print('portScanner.py [ip] [port Range start] [port range end]')
-    print('Example\nportScanner.py 192.168.100.8')
-    print('portScanner.py 192.168.100.8 8080')
-    print('portScanner.py 192.168.100.8 80 443')
-
-
-def main():
-
-    socket.setdefaulttimeout(0.1)
-    scan = scanner()
-    try:
-        if(len(sys.argv) == 2):
-            # scan fav ports
-            scan.favouriteScan(sys.argv[1])
-        elif(len(sys.argv) == 3):
-            # scan specific port
-            scan.portScan(sys.argv[1], int(sys.argv[2]))
-        elif(len(sys.argv) == 4):
-            # scan port range
-            scan.rangeScan(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+options = get_arguments()
+if sys.argv[1]:
+    if options.portStart and options.portEnd:
+        if options.stealthScan:
+            StealthScan(sys.argv[1], [int(options.portStart), int(options.portEnd)])
+        elif options.xmasScan:
+            XmasScan(sys.argv[1], [int(options.portStart), int(options.portEnd)])
         else:
-            help()
-
-    except Exception:
-        help()
-
-
-if __name__ == '__main__':
-    main()
+            FullScan(sys.argv[1], [int(options.portStart), int(options.portEnd)])
+    elif options.portStart:
+        if options.stealthScan:
+            StealthScan(sys.argv[1], [int(options.portStart)])
+        elif options.xmasScan:
+            XmasScan(sys.argv[1], [int(options.portStart)])
+        else:
+            FullScan(sys.argv[1], [int(options.portStart)])
+    else:
+        if options.stealthScan:
+            StealthScan(sys.argv[1], None)
+        elif options.xmasScan:
+            XmasScan(sys.argv[1], None)
+        else:
+            FullScan(sys.argv[1], None)
+else:
+    print("[-] No ip specified")
